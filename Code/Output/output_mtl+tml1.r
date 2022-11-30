fdf3d4e13498552eb5ed9fc665c91091c5df025b
Karan Vyas <karankvyas17@gmail.com>
2022-11-30 01:42:06 -0500

    update files

--- Code/Output/output_mtl+tml1.r
+++ Code/Output/output_mtl+tml1.r
@@ -1,11 +1,11 @@
 library(Metrics)
-precision_error = read.csv("/Users/karan/OneDrive/Desktop/TML/201020/predictions_base.csv")
-select = precision_error[,c(4,5,6)]
+precision_error = read.csv("/Users/karan/OneDrive/Desktop/TML/Data/201020/prediction_mtl_tml.csv")
+select = precision_error[,c(4,5,6,7)]
 
 select <- within(select, precision_error <- (abs((truth - response))/truth)*100 )
 #select <- within(select, RMSE <- rmse(truth,response))
 
-write.csv(select,"/Users/karan/OneDrive/Desktop/TML/Output/output_tml1_csv.csv", row.names = FALSE)
+write.csv(select,"C:/Users/karan/OneDrive/Desktop/TML/Code/Output/output_mtl+tmls1_csv.csv", row.names = FALSE)
 
 x = mean(select[,4])
 print(x)
--- Code/Output/output_tml+base1.r
+++ Code/Output/output_tml+base1.r
@@ -1,11 +1,12 @@
 library(Metrics)
-precision_error = read.csv("/Users/karan/OneDrive/Desktop/TML/201020/predictions_base.csv")
-select = precision_error[,c(4,5,6)]
+precision_error = read.csv("C:/Users/karan/OneDrive/Desktop/TML/Data/201020/prediction_tml_base.csv")
+select = precision_error[,c(4,5,6,7)]
+#print(select)
 
 select <- within(select, precision_error <- (abs((truth - response))/truth)*100 )
 #select <- within(select, RMSE <- rmse(truth,response))
 
-write.csv(select,"/Users/karan/OneDrive/Desktop/TML/Output/output_tml1_csv.csv", row.names = FALSE)
+write.csv(select,"C:/Users/karan/OneDrive/Desktop/TML/Code/Output/output_tml+base1_csv.csv", row.names = FALSE)
 
 x = mean(select[,4])
 print(x)
